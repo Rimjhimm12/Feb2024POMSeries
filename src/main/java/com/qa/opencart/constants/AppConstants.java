891c26fa7a48dc24fe80fa3cea888451908c7aa2
Rimjhimgit-1 <rimjhimmallick6@gmail.com>
2024-07-15 22:16:09 +0530

    updated

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -20,7 +20,7 @@ public class AppConstants {
 	
 	
 	
-	public static final String LOGIN_PAGE_TITLE= "Account Login";
+	public static final String LOGIN_PAGE_TITLE= "Account Login11";
 	public static final String LOGIN_PAGE_FRACTION_URL= "login";
 	public static final String ACCOUNT_PAGE_TITLE= "My Account";
 	public static final String ACCOUNT_PAGE_FRACTION_URL = "account";
